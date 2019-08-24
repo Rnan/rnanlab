56715ae665e15f4b442bc63faf7af6adc57a0240
Rnan <iq181@hotmail.com>
2019-08-24 23:44:45 +0700

    add composer generate QRCode (simple software io)

--- www/resources/views/index.blade.php
+++ www/resources/views/index.blade.php
@@ -6,12 +6,15 @@
 
 @section('func_content')
       <div>
-
+        <div class="visible-print text-center">
+        	{!! QrCode::size(100)->generate(Request::url()); !!}
+        	<p>Scan me to return to the original page.</p>
+        </div>
       </div>
       <div class="card-body">
         <h2 class="card-title">Free online services</h2>
         <p class="card-text">
-          
+
         </p>
       </div>
 @endsection
