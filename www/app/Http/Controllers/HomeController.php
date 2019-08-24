b1401d4f6938ad9475927221fe169a9d75ac987a
Rnan <iq181@hotmail.com>
2019-08-24 20:46:13 +0700

    add theme blog

--- www/app/Http/Controllers/HomeController.php
+++ www/app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-      return view('home',['str' => '']);
+      return view('index',['str' => '']);
     }
 
     public function encrypt(Request $request){
