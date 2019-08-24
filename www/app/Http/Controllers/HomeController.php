39d6c868d3e699e92889d388b822fa6e4297601c
Rnan <iq181@hotmail.com>
2019-08-24 23:00:26 +0700

    add log

--- www/app/Http/Controllers/HomeController.php
+++ www/app/Http/Controllers/HomeController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index(){
+      //Log::debug('## HomeController index');
       return view('index',['str' => '']);
     }
 
 
 
     public function encryptBase64(){
+      //Log::debug('## HomeController encryptBase64');
       return view('encryptBase64',['str' => '']);
     }
 
     public function encrypt(Request $request){
-
+      //Log::debug('## HomeController encrypt');
       $str = base64_encode($request->input('input'));
       return view('encryptBase64',['str' => $str]);
     }
@@ -25,11 +28,12 @@ class HomeController extends Controller
 
 
     public function decryptBase64(){
+      //Log::debug('## HomeController decryptBase64');
       return view('decryptBase64',['str' => '']);
     }
 
     public function decrypt(Request $request){
-
+      //Log::debug('## HomeController decrypt');
       $str = base64_decode($request->input('input'));
       return view('decryptBase64',['str' => $str]);
     }
