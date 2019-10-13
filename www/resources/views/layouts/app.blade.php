e1d628e1e6ce63f546ea58b5014ae8149b30c524
Rnan <iq181@hotmail.com>
2019-10-13 22:21:36 +0700

    edit website name to footer

--- www/resources/views/layouts/app.blade.php
+++ www/resources/views/layouts/app.blade.php
@@ -101,7 +101,7 @@
   <!-- Footer -->
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+      <p class="m-0 text-center text-white">Copyright &copy; RNANLAB.COM 2019</p>
       <p class="m-0 text-center text-white">Version {{ env('APP_VERSION').' '.env('APP_STATE') }}</p>
     </div>
     <!-- /.container -->
